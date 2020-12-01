7752e98407d6c0539ce01f872e3e58b86c496334
Eric Wahome <ericwahome20@gmail.com>
2020-12-01 09:41:51 +0300

    define mutation resolvers

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -2,6 +2,12 @@ const Query = {
   getAllUsers: async (root, args, ctx, info) => {},
   getAllVisits: async (root, args, ctx, info) => {},
 };
-const Mutation = {};
+const Mutation = {
+  signUp: async (root, args, context, info) => {},
+  signIn: async (root, args, context, info) => {},
+
+  addAssessment: async (root, args, context, info) => {},
+  updateAssessment: async (root, args, context, info) => {},
+};
 
 module.exports = { Query, Mutation };
