136e0e73aaa1cdd683dfd49bb2cbb124d5b6ac98
Eric Wahome <ericwahome20@gmail.com>
2020-12-01 08:45:38 +0300

    update server imports and configuration

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const { ApolloServer, gql } = require("apollo-server");
+const resolvers = require("./graphql/resolvers");
+const fs = require("fs");
+const typeDefs = gql(
+  fs.readFileSync("./server/graphql/schema.gql", { encoding: "utf-8" })
+);
 
 let port = process.env.PORT || 8080;
 
 const serverConfig = {
+  typeDefs,
+  resolvers,
   playground: true,
   introspection: true,
   endpoint: "/graphQL",
