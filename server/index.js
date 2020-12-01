6ed5980c08373985feb264e69993e82a67a2def9
Eric Wahome <ericwahome20@gmail.com>
2020-12-01 08:21:13 +0300

    configure and initialize apollo server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
 const { ApolloServer, gql } = require("apollo-server");
+
+let port = process.env.PORT || 8080;
+
+const serverConfig = {
+  playground: true,
+  introspection: true,
+  endpoint: "/graphQL",
+};
+
+const server = new ApolloServer(serverConfig);
+
+server.listen({ port }).then(({ url }) => {
+  console.log(`🚀 server ready at ${url}`);
+});
