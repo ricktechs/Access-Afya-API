43d7a2f47cf4b29179a8f17c8dee099c51524ad8
Eric Wahome <ericwahome20@gmail.com>
2020-12-02 11:47:26 +0300

    convert issues field to issueSchema| create a ref

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const issueSchema = new Schema({
+  badPrescription: Number,
+  labDelay: Number,
+  lateCheckIn: Number,
+  openedLate: Number,
+  carelessWasteDisposal: Number,
+  prescription: Number,
+  visitId: { type: Schema.Types.ObjectId },
+  staffId: { type: Schema.Types.ObjectId },
+  createdAt: { type: Date, default: Date.now },
+});
+
 const userSchema = new Schema({
   name: String,
   email: { type: String, unique: true },
@@ -16,28 +28,21 @@ const visitSchema = new Schema(
       type: String,
       unique: true,
     },
-
-    issues: [
-      {
-        type: new Schema({
-          badPrescription: Number,
-          labDelay: Number,
-          lateCheckIn: Number,
-          openedLate: Number,
-          carelessWasteDisposal: Number,
-          prescription: Number,
-          staffId: String,
-          createdAt: { type: Date, default: Date.now },
-        }),
-      },
-    ],
     patients: [{ type: new Schema({ count: Number }, { timestamps: true }) }],
     ratings: [{ type: new Schema({ nps: Number }, { timestamps: true }) }],
   },
-  { timestamps: true }
+  { timestamps: true, toObject: { virtuals: true } }
 );
+
+visitSchema.virtual("issues", {
+  ref: "Issue",
+  localField: "_id",
+  foreignField: "visitId",
+  justOne: false,
+});
 
 const User = mongoose.model("User", userSchema);
 const Visit = mongoose.model("Visit", visitSchema);
+const Issue = mongoose.model("Issue", issueSchema);
 
-module.exports = { User, Visit };
+module.exports = { User, Visit, Issue };
